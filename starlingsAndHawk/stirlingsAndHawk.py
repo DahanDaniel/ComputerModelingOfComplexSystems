7cc394d7b5b2fb33e0534ee08fef7bbb258e072d
Dahan, Daniel <daniel.dahan@merck.com>
2021-11-14 11:10:50 +0100

    Fixed periodicity of the Hawk

--- starlingsAndHawk/stirlingsAndHawk.py
+++ starlingsAndHawk/stirlingsAndHawk.py
@@ -6,109 +6,112 @@ from scipy.spatial import cKDTree
 import matplotlib.pyplot as plt
 import cv2
 
-L  = 32
-N  = 10000
-R  = 1
+L = 32
+N = 10000
+R = 1
 RB = 4
 V0 = 2
-A  = .15
-DT = 1
+A = 0.15
+DT = 0.3
 
 NO_FRAMES = 200
 FPS = 6
 DPI = 300
 
-WITH_PREDAOTR = False
+WITH_PREDAOTR = True
 
 PATH_PREFIX = os.path.dirname(os.path.realpath(__file__))
 
-class Birds():
-    def __init__(self, withPredator = False):
-        self.positions = (np.random.uniform(0, L, N)
-                          + 1j*np.random.uniform(0, L, N))
-        self.directions = np.random.uniform(-np.pi, np.pi, N)
-        self.velocities = V0 * np.exp(1j*self.directions)
 
-        positions2D = np.column_stack((self.positions.real,
-                                       self.positions.imag))
-        neighboursTree = cKDTree(positions2D, boxsize=[L,L])
-        self.neighboursDistances = np.array(neighboursTree.sparse_distance_matrix(
-            neighboursTree,
-            max_distance=R,
-            output_type='coo_matrix'
-            ).todense()).ravel()
+class Birds:
+    def __init__(self, withPredator=False):
+        self.positions = np.random.uniform(0, L, N) + 1j * np.random.uniform(0, L, N)
+        self.directions = np.random.uniform(-np.pi, np.pi, N)
+        self.velocities = V0 * np.exp(1j * self.directions)
+
+        positions2D = np.column_stack((self.positions.real, self.positions.imag))
+        self.neighboursTree = cKDTree(positions2D, boxsize=[L, L])
+        self.neighboursDistances = np.array(
+            self.neighboursTree.sparse_distance_matrix(
+                self.neighboursTree, max_distance=R, output_type="coo_matrix"
+            ).todense()
+        ).ravel()
         self.neighboursDistances = np.reshape(self.neighboursDistances, (N, N))
 
         self.withPredator = withPredator
         if self.withPredator:
-            self.predatorPos = (np.random.uniform(0, L)
-                                + 1j*np.random.uniform(0, L))
-            distancesToVictims = np.abs(self.positions - self.predatorPos)
-            closestVictimPos = self.positions[
-                # returns index of closest victim
-                np.argmin(distancesToVictims)
-                ]
-            self.predatorDir = np.angle(closestVictimPos
-                                        - self.predatorPos)
-            self.predatorVel = V0 * np.exp(1j*self.predatorDir)
+            self.predatorPos = np.random.uniform(0, L) + 1j * np.random.uniform(0, L)
+            closestVictimIdx = self.neighboursTree.query(
+                [self.predatorPos.real, self.predatorPos.imag], 1
+            )[1]
+            # distancesToVictims = np.abs(self.positions - self.predatorPos)
+            # closestVictimPos = self.positions[
+            #     # returns index of closest victim
+            #     np.argmin(distancesToVictims)
+            #     ]
+            # self.predatorDir = np.angle(closestVictimPos
+            #                             - self.predatorPos)
+            self.predatorDir = self.directions[
+                closestVictimIdx
+            ] + A * np.random.uniform(-np.pi, np.pi)
+            self.predatorVel = V0 * np.exp(1j * self.predatorDir)
 
     def new_directions(self):
         self.neighboursDistances[self.neighboursDistances > 0] = 1
         sum_velocities = np.add(
             np.array(np.dot(self.neighboursDistances, self.velocities)).ravel(),
-            self.velocities
-            )
-        new_directions = np.angle(sum_velocities) + A*np.random.uniform(-np.pi,
-                                                                        np.pi,
-                                                                        N)
+            self.velocities,
+        )
+        new_directions = np.angle(sum_velocities) + A * np.random.uniform(
+            -np.pi, np.pi, N
+        )
         return new_directions
 
     def update(self):
         self.directions = self.new_directions()
-        new_positions   = np.array(self.positions + DT*self.velocities)
-        new_velocities  = np.array(V0*np.exp(1j*self.directions))
+        new_positions = np.array(self.positions + DT * self.velocities)
+        new_velocities = np.array(V0 * np.exp(1j * self.directions))
 
         # Periodic conditions
         new_positions[new_positions.real > L] -= L
-        new_positions[new_positions.imag > L] -= 1j*L
+        new_positions[new_positions.imag > L] -= 1j * L
         new_positions[new_positions.real < 0] += L
-        new_positions[new_positions.imag < 0] += 1j*L
+        new_positions[new_positions.imag < 0] += 1j * L
 
         if self.withPredator:
             # Periodic conditions
-            predPosX = (self.predatorPos.real + DT*self.predatorVel.real) % L
-            predPosY = (self.predatorPos.imag + DT*self.predatorVel.imag) % L
+            predPosX = (self.predatorPos.real + DT * self.predatorVel.real) % L
+            predPosY = (self.predatorPos.imag + DT * self.predatorVel.imag) % L
             predPosX += L if predPosX < 0 else 0
             predPosY += L if predPosY < 0 else 0
-            self.predatorPos = predPosX + 1j*predPosY
+            self.predatorPos = predPosX + 1j * predPosY
             threatenedBirds = np.where(
                 # Birds in radius RB around predator
-                np.abs(self.positions - self.predatorPos) < RB
+                np.abs(self.positions - self.predatorPos)
+                < RB
             )
-            threatenedDir = np.angle(self.positions[threatenedBirds]
-                                     - self.predatorPos)
+            threatenedDir = np.angle(self.positions[threatenedBirds] - self.predatorPos)
             self.directions[threatenedBirds] = threatenedDir
-            new_velocities[threatenedBirds] = np.array(V0*np.exp(1j*threatenedDir))
-            
-            distancesToVictims = np.abs(self.positions - self.predatorPos)
-            closestVictimPos = self.positions[
-                # returns index of closest victim
-                np.argmin(distancesToVictims)
-                ]
-            self.predatorDir = np.angle(closestVictimPos
-                                        - self.predatorPos)
-            self.predatorVel = V0 * np.exp(1j*self.predatorDir)
+            new_velocities[threatenedBirds] = np.array(V0 * np.exp(1j * threatenedDir))
 
-        self.positions  = np.array(new_positions, dtype=complex)
+            closestVictimIdx = self.neighboursTree.query(
+                [self.predatorPos.real, self.predatorPos.imag], 1
+            )[1]
+            self.predatorDir = self.directions[
+                closestVictimIdx
+            ] + A * np.random.uniform(-np.pi, np.pi)
+            self.predatorVel = V0 * np.exp(1j * self.predatorDir)
+
+        self.positions = np.array(new_positions, dtype=complex)
         self.velocities = np.array(new_velocities, dtype=complex)
 
         positions2D = np.column_stack((self.positions.real, self.positions.imag))
-        neighboursTree = cKDTree(positions2D, boxsize=[L,L])
-        self.neighboursDistances = np.array(neighboursTree.sparse_distance_matrix(
-            neighboursTree,
-            max_distance=R,
-            output_type='coo_matrix'
-            ).todense()).ravel()
+        self.neighboursTree = cKDTree(positions2D, boxsize=[L, L])
+        self.neighboursDistances = np.array(
+            self.neighboursTree.sparse_distance_matrix(
+                self.neighboursTree, max_distance=R, output_type="coo_matrix"
+            ).todense()
+        ).ravel()
         self.neighboursDistances = np.reshape(self.neighboursDistances, (N, N))
 
     def save_frame(self):
@@ -119,26 +122,26 @@ class Birds():
         plt.clf()
         plt.quiver(X, Y, U, V, self.directions)
         if self.withPredator:
-            plt.quiver(self.predatorPos.real,
-                    self.predatorPos.imag,
-                    self.predatorVel.real,
-                    self.predatorVel.imag)
+            plt.quiver(
+                self.predatorPos.real,
+                self.predatorPos.imag,
+                self.predatorVel.real,
+                self.predatorVel.imag,
+            )
         plt.xlim([0, L])
         plt.ylim([0, L])
 
         i = 0
-        nameIndex = '000'
-        while os.path.exists(os.path.join(
-            PATH_PREFIX,
-            r"starlingsSimulation\\frame%s.png" % nameIndex
-            )):
+        nameIndex = "000"
+        while os.path.exists(
+            os.path.join(PATH_PREFIX, r"starlingsSimulation\\frame%s.png" % nameIndex)
+        ):
             i += 1
-            nameIndex = (3-len(str(i)))*'0' + str(i)
+            nameIndex = (3 - len(str(i))) * "0" + str(i)
         # leading zeros in frame name
-        nameIndex = (3-len(str(i)))*'0' + str(i)
+        nameIndex = (3 - len(str(i))) * "0" + str(i)
         figName = os.path.join(
-            PATH_PREFIX,
-            r"starlingsSimulation\\frame%s.png" % nameIndex
+            PATH_PREFIX, r"starlingsSimulation\\frame%s.png" % nameIndex
         )
         plt.savefig(figName, dpi=DPI)
 
@@ -149,23 +152,26 @@ class Birds():
         V = self.velocities.imag
         plt.quiver(X, Y, U, V, self.directions)
         if self.withPredator:
-            plt.quiver(self.predatorPos.real,
-                    self.predatorPos.imag,
-                    self.predatorVel.real,
-                    self.predatorVel.imag)
+            plt.quiver(
+                self.predatorPos.real,
+                self.predatorPos.imag,
+                self.predatorVel.real,
+                self.predatorVel.imag,
+            )
         plt.xlim([0, L])
         plt.ylim([0, L])
         plt.show()
 
+
 def create_video():
-    image_folder = os.path.join(PATH_PREFIX, 'starlingsSimulation')
-    video_name = os.path.join(PATH_PREFIX, 'video.avi')
+    image_folder = os.path.join(PATH_PREFIX, "starlingsSimulation")
+    video_name = os.path.join(PATH_PREFIX, "video.avi")
 
     images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
     frame = cv2.imread(os.path.join(image_folder, images[0]))
     height, width, layers = frame.shape
 
-    video = cv2.VideoWriter(video_name, 0, FPS, (width,height))
+    video = cv2.VideoWriter(video_name, 0, FPS, (width, height))
 
     for image in images:
         video.write(cv2.imread(os.path.join(image_folder, image)))
@@ -173,12 +179,13 @@ def create_video():
     cv2.destroyAllWindows()
     video.release()
 
+
 def main():
-    # Empty the frames directory    
-    files = glob.glob(os.path.join(PATH_PREFIX, 'starlingsSimulation/*'))
+    # Empty the frames directory
+    files = glob.glob(os.path.join(PATH_PREFIX, "starlingsSimulation/*"))
     for f in files:
         os.remove(f)
-    
+
     # Simulate and save
     swarm = Birds(WITH_PREDAOTR)
     swarm.save_frame()
@@ -187,5 +194,6 @@ def main():
         swarm.save_frame()
     create_video()
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     main()
